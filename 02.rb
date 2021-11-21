108c0784c9cf8b87fea66d09edd4caa6c935d913
Paul Nelson <panel@umich.edu>
2021-11-20 18:33:45 -0600

    day 2 part 2

--- 02.rb
+++ 02.rb
@@ -1,10 +1,17 @@
 data = File.open("02.txt").read.split("\n")
 
-
-d2 = data.map do |line| 
+p1 = []
+p2 = []
+d2 = data.each do |line| 
     s = /(?<min>\d+)-(?<max>\d+) (?<test>\w): (?<pw>\w+)/.match(line)
     char = s[:pw].chars.select { |c| c == s[:test] }.count
-    char >= s[:min].to_i && char <= s[:max].to_i
+    p1.push char >= s[:min].to_i && char <= s[:max].to_i
+    
+    t1 = s[:pw][s[:min].to_i - 1] == s[:test] 
+    t2 = s[:pw][s[:max].to_i - 1] == s[:test]
+
+    p2.push (!t1 == t2)
 end
 
-puts "Part 1: #{d2.select { |x| x == true }.count}"
+puts "Part 1: #{p1.select { |x| x == true }.count}"
+puts "Part 2: #{p2.select { |x| x == true }.count}"
