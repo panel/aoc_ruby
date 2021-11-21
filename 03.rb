4ab180d385d8ba3f79094a7a4457758088df2df6
Paul Nelson <panel@umich.edu>
2021-11-20 19:00:38 -0600

    day 3 part 2

--- 03.rb
+++ 03.rb
@@ -1,33 +1,38 @@
 data = File.open('03.txt').read.split("\n")
+strategies = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
 class Toboggan 
-    def initialize(xMax, yMax, path)
-        @xMax = xMax
-        @yMax = yMax
+    def initialize(dx, dy, path)
+        @xMax = path[0].length
+        @yMax = path.length
         @path = path
+        @dx = dx
+        @dy = dy
         @hits = 0
         @x = 0
         @y = 0
     end
 
     def next()
-        @y += 1
-        @x = (@x + 3) % @xMax
+        @y += @dy
+        @x = (@x + @dx) % @xMax
         @hits += 1 if !self.done? && @path[@y][@x] == '#'
-    end
-    
-    def hits()
-        @hits
     end
 
     def done?()
         @y >= @yMax
     end
+
+    def run()
+        while !self.done?
+            self.next()
+        end
+
+        @hits
+    end
 end
 
-toboggan = Toboggan.new(data[0].chars.count, data.count, data)
-while !toboggan.done?
-    toboggan.next
-end
-
-puts "Part 1: #{toboggan.hits}"
+pt1 = Toboggan.new(3, 1, data)
+pt2 = strategies.map { |s| Toboggan.new(s[0], s[1], data).run }
+puts "Part 1: #{pt1.run}"
+puts "Part 2: #{pt2.reduce(:*)}"
