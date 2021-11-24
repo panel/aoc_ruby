2971315940d2749feba802af62250c9b7cf74b76
Paul Nelson <panel@umich.edu>
2021-11-23 18:16:26 -0600

    day 6 part 2

--- 06.rb
+++ 06.rb
@@ -11,3 +11,17 @@ pt1 = data.reduce(0) do |sum, group|
 end
 
 puts "Part 1: #{pt1}"
+
+pt2 = data.reduce(0) do |sum, group| 
+    answer_hash = group.reduce({}) do |group_answers, answer|
+        answer.chars.each do |char|
+            group_answers[char] ||= 0
+            group_answers[char] += 1
+        end
+        group_answers
+    end
+    sum += answer_hash.values.select { |v| v == group.size }.size
+    sum
+end
+
+puts "Part 2: #{pt2}"
